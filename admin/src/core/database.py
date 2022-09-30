e573965848b9ab9252f4a20c8583d9fc4de3d0f8
Thomas Rusiecki <rusieckithomas@gmail.com>
2022-09-30 14:19:28 -0300

    Checks if table exists before first request

--- admin/src/core/database.py
+++ admin/src/core/database.py
@@ -8,10 +8,12 @@ def init_app(app):
 
 
 def config_db(app):
-    #TODO: change before deploying to production!
     @app.before_first_request
-    def init_database():
-        db.create_all()
+    def init_database():   
+        if not db.inspect(db.engine).has_table("user"):
+            reset_db()
+        else:
+            print('Database already exists!')
 
     @app.teardown_request
     def close_session(exception=None):
@@ -24,3 +26,8 @@ def reset_db():
     print('Creating all tables!')
     db.create_all()
     print('Database reset!')
+
+
+def drop_db():
+    print('Dropping all tables!')
+    db.drop_all()
