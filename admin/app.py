5337d4079d559fb236d63bf3d724e24515299095
David Franco <francodavid20@gmail.com>
2022-10-11 09:13:44 -0300

    ADd session type to session handling

--- admin/app.py
+++ admin/app.py
@@ -6,7 +6,7 @@ static_folder = Path(__file__).parent.joinpath('public')
 
 app = create_app(static_folder)
 app.secret_key = environ.get("FLASK_SECRET_KEY", 'this is just a secret')
-
+app.config['SESSION_TYPE'] = 'filesystem'
 
 def main():
   app.run()
