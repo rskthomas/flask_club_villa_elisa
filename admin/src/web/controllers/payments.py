46a9c54bd36a729e8787a345f6f5ae48695ff482
Thomas Rusiecki <rusieckithomas@gmail.com>
2022-10-15 01:28:52 -0300

    payments controller refactor

--- admin/src/web/controllers/payments.py
+++ admin/src/web/controllers/payments.py
@@ -10,7 +10,6 @@ from src.core import payments as Payments
 from datetime import date
 
 
-
 payments_blueprint = Blueprint("payments", __name__, url_prefix="/payments")
 
 
@@ -26,35 +25,29 @@ def search():
         flash("Hubo un error con el formulario", "danger")
         return redirect(url_for("payments.index"))
 
+
     input = form.search.data
-    print(input)
 
     if form.select.data == "member_id":
-        # If user is searched by member_id, either there is a member with that id or there is none
+        #TODO: validate that input is a number if member_id is selected
+        route = "payments.invoices"
         member = Member.search_by_id(input)
-        #TODO fix this: if input is not a number, it will return sql error 
-        if member:
-            print(f"member found: {member.first_name}")
-            return redirect(url_for("payments.invoices", id=input))
-
     elif form.select.data == "last_name":
-        # There could be multiple Gonzalez, it is unfair only one is paying
-        members = Member.search_by_last_name(input)
-        for member in members:
-            print(f"member found: {member.first_name}")
-        if members:
-            #TODO fix this: payments.results does not recognize both parameters
-            return redirect(
-                url_for("payments.results", last_name=input, members=members)
-            )
+        #TODO: This doesnt work if there is a match (missing 1 required positional argument: 'id')
+        route = "payments.results"
+        member = Member.search_by_last_name(input)
 
-    flash(f"No se han encontrado miembros con apellido {input}", "danger")
-    return redirect(url_for("payments.index"))
+    if not member:
+        flash("No se encontró ningún miembro con ese criterio", "danger")
+        return redirect(url_for("payments.index"))
+
+    return redirect(url_for(route, id=input))
 
 
-@payments_blueprint.get("/search/results/")
-def results(last_name, members):
-    print(f"last_name: {last_name}")
+@payments_blueprint.get("/search/results/<string:id>")
+def results(id):
+    last_name = id
+    members = Member.search_by_last_name(last_name)
     return render_template(
         "payments/results.html", members=members, last_name=last_name
     )
@@ -62,21 +55,26 @@ def results(last_name, members):
 
 @payments_blueprint.get("/<int:id>/invoices")
 def invoices(id):
-    member=Member.search_by_id(id)
+    member = Member.search_by_id(id)
 
-    #TODO this obviously has issues, check later how to do it
+    # TODO this obviously has issues, check later how to do it
     last_invoice = Payments.lastInvoice(id)
-    if last_invoice == date.today().month or (not last_invoice and date.today().day < 11):
+    if last_invoice == date.today().month or (
+        not last_invoice and date.today().day < 11
+    ):
         createInvoice(id)
 
     invoices = Payments.member_invoices(id)
     return render_template("payments/invoices.html", member=member, invoices=invoices)
 
+
 def createInvoice(member_id):
-    base_price= 100
-    total_price = base_price 
-    member_number= member_id
-    Payments.createInvoice(member_number=member_number, total_price=total_price, base_price=base_price)
+    base_price = 100
+    total_price = base_price
+    member_number = member_id
+    Payments.createInvoice(
+        member_number=member_number, total_price=total_price, base_price=base_price
+    )
 
 
 @payments_blueprint.get("/<int:id>/invoice/<invoice_id>")
