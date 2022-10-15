e2945787e2d3a1e68ddf0ebb3062cb8b4d086960
Thomas Rusiecki <rusieckithomas@gmail.com>
2022-10-14 23:02:09 -0300

    invoice and member model updated

--- admin/src/core/member/member.py
+++ admin/src/core/member/member.py
@@ -15,5 +15,5 @@ class Member(db.Model):
     phone_number        = db.Column(db.Integer, nullable=True)
     email               = db.Column(db.String(50), unique=True)
     activation_date     = db.Column(db.DateTime, default=datetime.now())
-    invoices             = db.relationship("Invoice", backref="member", lazy=True)
+    invoices            = db.relationship("Invoice", backref="member", lazy=True, cascade="all, delete-orphan", single_parent=True, nullable=True)
 
