1b95883e5b17e4fa89c74c2d407dd97cd5c13e56
Thomas Rusiecki <rusieckithomas@gmail.com>
2022-10-19 13:18:12 -0300

    extra_item list added on invoice summary

--- admin/src/core/payments/invoice.py
+++ admin/src/core/payments/invoice.py
@@ -7,19 +7,19 @@ class Invoice(db.Model):
     year = db.Column(db.Integer(), nullable=False, default=date.today().year)
     month = db.Column(db.Integer(), nullable=False, default=date.today().month)
     base_price = db.Column(db.String(50), nullable=False)
-    total_price = db.Column(db.String(50), nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
     paid = db.Column(db.Boolean(), default=False)
     member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
     member = db.relationship("Member", back_populates="invoices")
     expired = db.Column(db.Boolean(), default=False)
-    payments = db.relationship("Payment")
+    payment = db.relationship("Payment", back_populates="invoice", uselist=False)
     extra_items = db.relationship("InvoiceExtraItem")
 
 class InvoiceExtraItem(db.Model):
     """Invoice extra item model"""
     id = db.Column(db.Integer, primary_key=True, unique=True)
-    description = db.Column(db.String(50), nullable=False)
-    amount = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
     payment_date = db.Column(db.DateTime(), default=db.func.now())
     discipline_id = db.Column(db.Integer, db.ForeignKey('discipline.id'), nullable=True)
     invoice_id = db.Column(db.Integer, db.ForeignKey('invoice.id'), nullable=False)
--- admin/src/core/payments/payment.py
+++ admin/src/core/payments/payment.py
@@ -1,3 +1,4 @@
+from operator import inv
 from src.core.database import db
 
 
@@ -5,6 +6,7 @@ class Payment(db.Model):
     """Payment model"""
     id = db.Column(db.Integer, primary_key=True, unique=True)
     invoice_id = db.Column(db.Integer, db.ForeignKey('invoice.id'), nullable=False)
+    invoice = db.relationship("Invoice", back_populates="payment")
     member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
     amount = db.Column(db.String(50), nullable=False)
     payment_date = db.Column(db.DateTime(), default=db.func.now())
