ab88ba7885e88f92136b75acee47d2a6bd1cdd99
Thomas Rusiecki <rusieckithomas@gmail.com>
2022-10-19 11:30:31 -0300

    invoice model updated

--- admin/src/core/payments/invoice.py
+++ admin/src/core/payments/invoice.py
@@ -2,20 +2,25 @@ from src.core.database import db
 from datetime import date
 
 class Invoice(db.Model):
+    """Invoice model"""
     id = db.Column(db.Integer, primary_key=True, unique=True)
     year = db.Column(db.Integer(), nullable=False, default=date.today().year)
     month = db.Column(db.Integer(), nullable=False, default=date.today().month)
     base_price = db.Column(db.String(50), nullable=False)
     total_price = db.Column(db.String(50), nullable=False)
     paid = db.Column(db.Boolean(), default=False)
-    member_number = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
-    payment = db.relationship("Payment", backref="invoice", lazy=True)
-    #TODO implement extra items when discipline enrollment is finished
+    member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
+    member = db.relationship("Member", back_populates="invoices")
+    expired = db.Column(db.Boolean(), default=False)
+    payments = db.relationship("Payment")
+    extra_items = db.relationship("InvoiceExtraItem")
 
-class invoice_extra_item(db.Model):
+class InvoiceExtraItem(db.Model):
+    """Invoice extra item model"""
     id = db.Column(db.Integer, primary_key=True, unique=True)
     description = db.Column(db.String(50), nullable=False)
     amount = db.Column(db.String(50), nullable=False)
     payment_date = db.Column(db.DateTime(), default=db.func.now())
-    discipline_id = db.Column(db.Integer, db.ForeignKey('discipline.id'), nullable=False)
+    discipline_id = db.Column(db.Integer, db.ForeignKey('discipline.id'), nullable=True)
     invoice_id = db.Column(db.Integer, db.ForeignKey('invoice.id'), nullable=False)
+    
