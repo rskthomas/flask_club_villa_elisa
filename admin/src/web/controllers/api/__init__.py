04b5dbafa8a8d750b514dc5da3942dcabb8115cb
Thomas Rusiecki <rusieckithomas@gmail.com>
2022-10-20 19:12:37 -0300

    api for GET list of disciplines added

--- admin/src/web/controllers/api/__init__.py
+++ admin/src/web/controllers/api/__init__.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
-from src.core import member
 from src.web.controllers.api.members import member_api_blueprint
+from src.web.controllers.api.club import club_api_blueprint
 
 api_blueprint = Blueprint("api", __name__, url_prefix="/api")
 
 api_blueprint.register_blueprint(member_api_blueprint)
+
+api_blueprint.register_blueprint(club_api_blueprint)
---
+++ admin/src/web/controllers/api/club.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request
+from src.web.helpers.handlers import bad_request
+from src.core.discipline import get_disciplines
+
+
+
+club_api_blueprint = Blueprint("club_api", __name__, url_prefix="/club")
+
+def discipline_as_json(discipline):
+    return {
+        'id': discipline.id,
+        'category': discipline.category,
+        'coach' : discipline.coach,
+        'schedule': discipline.schedule,
+        'monthly_price': discipline.monthly_price,
+        'active': discipline.active,
+        'created_at': discipline.created_at
+    }
+
+
+@club_api_blueprint.get('/disciplines')
+def index():
+    disciplines = get_disciplines()
+    return { 'disciplines': list(map(discipline_as_json, disciplines))}
+
