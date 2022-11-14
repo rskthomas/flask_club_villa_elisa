c0bce04cd72bdb4d16388459e12cfc907d5cc202
nbarone <nicolasbarone@gmail.com>
2022-11-14 17:23:31 -0300

    Add LoginView

--- portal/src/views/LoginView.vue
+++ portal/src/views/LoginView.vue
@@ -1,24 +1,53 @@
 
 <template>
   <form @submit.prevent="submit">
-        <h1 class="h3 mb-3 fw-normal">Login</h1>
+    <h1 class="h3 mb-3 fw-normal">Login</h1>
 
-        <div class="form-floating">
+    <div class="form-floating">
           <input type="username" class="form-control" placeholder="username" required>
           <label for="floatingInput">Usuario</label>
         </div>
 
         <div class="form-floating">
           <input type="password" class="form-control" placeholder="Password" required>
-          <label for="floatingPassword">Password</label>
+          <label for="floatingPassword">Clave</label>
         </div>
-        
-        <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
-    </form>
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+  </form>
 </template>
 
 
-<script setup>
+<script lang="ts">
+import {reactive} from 'vue';
+import {useRouter} from "vue-router";
+
+export default {
+  name: "Login",
+  setup() {
+    const data = reactive({
+      username: '',
+      password: ''
+    });
+
+    const router = useRouter();
+
+    const submit = async () => {
+      await fetch('http://localhost:5001/api/auth/login', {
+        method: 'POST',
+        body:JSON.stringify(data),
+        mode:"no-cors",
+        headers: {"Content-type":"application/json", "Access-Control-Allow-Origin": "*"}
+      });
+      await router.push('/');
+    }
+    
+    return {
+      data,
+      submit
+    }
+  }
+}
 </script>
 
 <style>
