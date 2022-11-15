ee8c7c40e0a5551bf49bc5c8c6c61daa00b78792
Thomas Rusiecki <rusieckithomas@gmail.com>
2022-11-15 14:18:11 -0300

    disciplines api update

--- admin/src/web/controllers/api/club.py
+++ admin/src/web/controllers/api/club.py
@@ -5,36 +5,38 @@ from src.core.discipline import get_disciplines
 
 club_api_blueprint = Blueprint("club_api", __name__, url_prefix="/club")
 
-email= "clubdeportivovillaelisa@gmail.com"
-phone= "0221 487-0193"
+email = "clubdeportivovillaelisa@gmail.com"
+phone = "0221 487-0193"
+
 
 def discipline_as_json(discipline):
     """Converts a discipline to json and returns it"""
     return {
         "id": discipline.id,
+        "name": discipline.name,
         "category": discipline.category,
         "coach": discipline.coach,
         "schedule": discipline.schedule,
         "monthly_price": discipline.monthly_price,
-        "active": discipline.active,
-        "created_at": discipline.created_at,
+        "active": discipline.active
     }
 
 
 @club_api_blueprint.get("/disciplines")
 def disciplines():
     disciplines = get_disciplines()
-    response = make_response(jsonify({"disciplines": list(map(discipline_as_json, disciplines))}), 200)
-    response.headers['Content-Type'] = 'application/json'
+    response = make_response(
+        jsonify([discipline_as_json(discipline) for discipline in disciplines]), 200
+    )
+    response.headers["Content-Type"] = "application/json"
 
     return response
+
 
 @club_api_blueprint.get("/info")
 def info():
     response = make_response(jsonify({"email": email, "phone": phone}), 200)
-    response.headers['Content-Type'] = 'application/json'
-    response.headers['Schema'] = """asdasd"""
-
+    response.headers["Content-Type"] = "application/json"
+    response.headers["Schema"] = """asdasd"""
 
     return response
-
