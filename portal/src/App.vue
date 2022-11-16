d552ce9ef5c11c35ae267e5e30a74d7e33499e69
David Franco <francodavid20@gmail.com>
2022-11-16 10:08:42 -0300

    Add initial homepage

--- portal/src/App.vue
+++ portal/src/App.vue
@@ -1,18 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import NavbarView from "./components/NavbarView.vue";
+import Footer from "./components/Footer.vue";
+</script>
 <template>
-
   <NavbarView />
-
   <main id="parent">
     <RouterView/>
   </main>
-
+  <Footer />
 </template>
-
-
-<script setup>
-  import { RouterLink, RouterView } from 'vue-router'
-  import NavbarView from './components/NavbarView.vue'
-</script>
 
 <style>
 /*this will center all components inside main. Im not good with css so this is the only way i made it work
