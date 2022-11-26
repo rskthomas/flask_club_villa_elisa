fef9b762520953bc3e8c43841267b2f42d7e682b
Thomas Rusiecki <rusieckithomas@gmail.com>
2022-11-26 01:40:35 -0300

    comment correction

--- portal/src/main.js
+++ portal/src/main.js
@@ -5,6 +5,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router) 
-/*global variable to set api URL. USAGE on <setup>: const apiURL = inject('ENDPOINT_PATH'), importing inject from vue */
+/*global variable to set api URL. USAGE on <script setup>: const apiURL = inject('ENDPOINT_PATH'), importing inject from vue */
 app.provide('ENDPOINT_PATH', "http://localhost:5000/api")
 app.mount('#app')
