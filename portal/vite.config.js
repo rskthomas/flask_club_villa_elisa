d568991c3d5d9f7758e9614241bab56b275bc335
Thomas Rusiecki <rusieckithomas@gmail.com>
2022-11-26 01:35:24 -0300

    vite server runs on port 3000

--- portal/vite.config.js
+++ portal/vite.config.js
@@ -13,14 +13,6 @@ export default defineConfig({
     }
   },
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,      
-        ws: true,
-      }
-    }
+    port: 3000
   }
 })
