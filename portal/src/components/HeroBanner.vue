601b14f0e6a68969e5e87a265ae8670451dfb34d
Thomas Rusiecki <rusieckithomas@gmail.com>
2022-11-27 15:13:44 -0300

    minor home page changes

--- portal/src/components/HeroBanner.vue
+++ portal/src/components/HeroBanner.vue
@@ -4,7 +4,7 @@
 <template>
   <div class="container-fluid">
     <div class="row">
-      <div class="col">
+      <div class="col items">
         <h1>Club Deportivo Villa Elisa</h1>
         <h2>Bienvenido al Portal de Gestión Online</h2>
         <ul>
@@ -18,3 +18,32 @@
     </div>
     </div>
 </template>
+<style>
+
+h1 {
+  font-size: 3rem;
+  font-weight: 700;
+  text-align: center;
+}
+h2 {
+  font-size: 1.5rem;
+  font-weight: 700;
+  text-align: center;
+  margin-top: 0.5rem;
+  margin-bottom: 3rem;
+}
+.col {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+li {
+  font-size: 0.9rem;
+  font-weight: 700;
+  color: #008040;
+  text-align: left;
+  margin-bottom: 0.5rem;
+}
+
+</style>
