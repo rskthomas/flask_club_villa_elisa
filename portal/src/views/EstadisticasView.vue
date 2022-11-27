242c770fc773190ac0a903ba1abdf8ec897bc16d
David Franco <francodavid20@gmail.com>
2022-11-27 11:15:42 -0300

    More fixes on permissions

--- portal/src/views/EstadisticasView.vue
+++ portal/src/views/EstadisticasView.vue
@@ -4,35 +4,37 @@ import InvoicesChart from "../components/InvoicesChart.vue";
 import MembersByGenderChart from "../components/MembersByGenderChart.vue";
 import auth from "../auth";
 import { useRouter } from "vue-router";
-
+import { ref } from "vue";
 const router = useRouter();
+const enoughPermissions = ref(false);
 
 const checkAccessLevel = async () => {
   let user = await auth.currentUser();
 
-  console.log(user);
   if (!user.id) {
     alert("usted debe iniciar sesión para ver esta pantalla");
     router.push("/login");
   } else if (!auth.isAdmin(user) && !auth.isOperador(user)) {
     alert("Usted no tiene permisos suficientes");
     window.location.href = "/";
+  } else {
+    enoughPermissions.value = true;
   }
 };
 
-await checkAccessLevel();
+checkAccessLevel();
 </script>
 
 <template>
   <div class="row">
     <div class="col">
-      <DisciplinesChart></DisciplinesChart>
+      <DisciplinesChart v-if="enoughPermissions"></DisciplinesChart>
     </div>
     <div class="col">
-    <MembersByGenderChart></MembersByGenderChart>
+    <MembersByGenderChart v-if="enoughPermissions"></MembersByGenderChart>
     </div>
     <div class="col">
-      <InvoicesChart></InvoicesChart>
+      <InvoicesChart v-if="enoughPermissions"></InvoicesChart>
     </div>
   </div>
 </template>
