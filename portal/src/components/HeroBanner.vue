6d8da52f4d0162fcf3982a031db178294e7ae8ac
Thomas Rusiecki <rusieckithomas@gmail.com>
2023-06-14 02:32:56 -0300

    home logo is now responsive

--- portal/src/components/HeroBanner.vue
+++ portal/src/components/HeroBanner.vue
@@ -13,7 +13,15 @@
         </ul>
      </div>
       <div class="col">
-        <img src="/logo.png" />
+
+        <img srcset="/pwa-192x192.png 320w,
+             /pwa-icon-192x192.png 192w,
+             /logo.png 800w"
+          sizes="(max-width: 320px) 200px,
+                  (max-width: 480px) 400px,
+                  800px"
+          src="/logo.png" alt="logo">
+
       </div>
     </div>
     </div>
